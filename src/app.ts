a2aabeaba4121c385d00df9f318414da07759580
Git cua Hung <nguyenthinhhung150603@gmail.com>
2025-05-14 11:57:33 +0700

    video 34

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 // const express = require("express");
 import express from "express";
 import "dotenv/config";
+import webRoutes from "./routes/web";
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -9,9 +10,8 @@ const port = process.env.PORT || 8080;
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
+//config routes
+webRoutes(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
